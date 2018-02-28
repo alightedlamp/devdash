4f7f5dc37d6dd186ea79fae8221a2d7ff01665fc
Philip Harrell <alightedlamp@gmail.com>
2018-02-28 17:26:50 -0600

    Update dashboard client JS test sketch

--- test/devdashClientTest.js
+++ test/devdashClientTest.js
@@ -3,4 +3,10 @@ process.env.NODE_ENV = 'test';
 const expect = require('chai').expect;
 const script = require('../src/public/js/devdash.js');
 
-describe('Front end scripts', function() {});
+describe('Client-side JS', function() {
+  describe('Dashboard Page', function() {});
+
+  describe('Project Page', function() {});
+
+  describe('Resource page', function() {});
+});
