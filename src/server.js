3682a0515886d8480c6e1a39636e7e338c2faae6
Cecil Habermacher <29548074+chabermacher@users.noreply.github.com>
2018-02-27 20:29:41 -0600

    Commented out /resource and /user since they're not used. Left them in the code just in case we decide to add a page.

--- src/server.js
+++ src/server.js
@@ -51,11 +51,12 @@ app.use('/', root);
 const project = require('./controllers/project.js');
 app.use('/project', project);
 
-const resource = require('./controllers/resource.js');
-app.use('/resource', resource);
+// Pages below are not needed. Rather than deleting them, I'm stubbing them out in case they're ever needed in the future.
+// const resource = require('./controllers/resource.js');
+// app.use('/resource', resource);
 
-const user = require('./controllers/user.js');
-app.use('/user', user);
+// const user = require('./controllers/user.js');
+// app.use('/user', user);
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
