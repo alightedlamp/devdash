2ad9c1d324f04d9d231e6a2ca93937b765ef131f
Philip Harrell <alightedlamp@gmail.com>
2018-03-03 08:06:36 -0600

    Update keys

--- config/keys.js
+++ config/keys.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   sessionSecret: process.env.SESSION_SECRET,
   jawsDbUser: process.env.JAWS_DB_USER,
+  jawsDbName: process.env.JAWS_DB,
   jawsDbPassword: process.env.JAWS_DB_PASSWORD,
   jawsDbHost: process.env.JAWS_DB_HOST
 };
